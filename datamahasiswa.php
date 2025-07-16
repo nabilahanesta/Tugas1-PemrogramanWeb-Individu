750d06fbf848867d38733a63ba3deca259d582d0
nabilahanesta <nabilahanestap09@gmail.com>
2025-07-16 12:28:50 +0700

    menambahkan register dan session

--- datamahasiswa.php
+++ datamahasiswa.php
@@ -1,91 +1,71 @@
-<?php //untuk membuka, namanya delimiter yang dibawah, tag penutup di php (tag khusus php)
-    
-    //mengkoneksikan dengan database cara menhubungkan nya pake IPA interprice 
+<?php
+   // $nama = "Ratih Khotimahtus S";
+   // echo "Hello World" . $nama;
 
-    // ada 4 parameter : localhost, username (root), password, nama database (di local host perhatikan port nya, kalo port default 3306 gini "localhost" kalo portnya beda bukan 3306 gini "localhost:8111" )
-    //buat variabel dulu biar efektif ngoding pas if nya jd singkat (ini mirip class di css)
-    //$koneksi = mysqli_connect("localhost:8111", "root", "", "webif");
+   session_start();
 
-    //cara mengecek kalo udh connect blm ke database
-    //if(!$koneksi)
-    //{
-        //die("Koneksi Gagal!" .mysqli_connect_error());
-    //}
-    //else
-    //{
-        //echo "Koneksi Berhasil!";
-    //}
+   if(!isset($_SESSION["login"]))
+   {
+    header("Location: login1.php");
+    exit;
+   }
 
-    //$query = "SELECT * FROM mahasiswa";
+   include 'function.php';
+   $query = "SELECT * FROM mahasiswa";
+   $rows = query($query); /// hasilnya wadah dengan isinya
 
-    //$result = mysqli_query($koneksi, $query); //ini adalah objek 
-    
-    // echo gabisa menampilkan objek 
-    // echo $result; *coding
+   /// ambil data (fetch) dari lemari result
+    /// while ($mhs = mysqli_fetch_assoc($result))
 
-    //cara ambil data dalemnya, namanya (fetch) dari lemari result
-    // cara nya :
-    // mysqli_fetch_row() untuk array tp numerik 
-    // mysqli_fetch_assoc() ini buat asosiatif 
-    // mysqli_fetch_array() bisa assos bisa row tp ada kendala klo ga manggil 1 data aja nanti munculnya 2 kali
-    // mysqli_fetch_object() untuk objek, pake id tp tipe nya objek hrs pake tanda panah 
-    // var_dump($mhs->nama); *codingan buat objek 
+   /// mysqli_fetch_row() untuk array tipe numerik
+   /// mysqli_fetch_assoc() untuk assosiatif
+   /// mysqli_fetch_array()
+   /// mysqli_fetch_object() pakai var_dump($mhs->nama);
+    ///{
+      ///  var_dump($mhs);
+        /// kalo mau ditampilkan isi table tertentu var_dump($mhs["nim"]); 
+    ///}
+   
+?>
 
-    //cara ngambil data yg muncul cuma 1 yg paling atas 
-    //$mhs = mysqli_fetch_row($result);
-    
-    //kalo mau nampilin 1 data aja 
-    //var_dump($mhs[1]); *coding ([1] ini id nya)
-    ////var_dump($mhs["nama"]); *coding ([1] ini index nya) kalo mau nampilin nama aja 
-    //var_dump($mhs[1]);
-
-    //while ($mhs = mysqli_fetch_assoc($result))
-
-    //var_dump($mhs); 
-
-    //kaya css bisa conect di 1 halaman lebih efektif 
-    require 'function.php';
-    $query = "SELECT * FROM mahasiswa";
-    $rows = query($query); //hasilnya wadah dengan isinya 
-    
-?>  
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+    <title>DATA MAHASISWA</title>
 </head>
 <body>
-    <h1> Data Mahasiswa </h1>
-    <a href="tambahdata.php"><button style="margin-bottom: 12px; background-color :lightblue;" >Tambah Data</button></a>
+    <a href="logout.php"> Logout</a>
+    <h1>Data Mahasiswa</h1>
     
+    <a href= "tambahdata.php"><button style="margin-bottom: 12px; background-color: lightgreen;">Tambah Data</button></a>
+
     <table border="1" cellspacing="0" cellpadding="10">
         <tr>
-            <th> No </th>
-            <th> Foto </th>
-            <th> Nama </th>
-            <th> NIM </th>
-            <th> Jurusan </th>
-            <th> No HP </th>
-            <th> Aksi </th>
+            <th>No</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+            <th>No Hp</th>
+            <th>Aksi</th>
         </tr>
-        <?php 
+        <?php
         $i = 1;
         foreach ($rows as $mhs) { ?>
         <tr>
             <td><?= $i ?></td>
-            <td><img src="images/<?=$mhs['foto'];?>" alt="<?=$mhs['nama'];?>" width="100"></td>
-            <td><?= $mhs['nama']; ?></td>
-            <td><?= $mhs['nim']; ?></td>
-            <td><?= $mhs['jurusan']; ?></td>
-            <td><?= $mhs['nohp']; ?></td>
-            <td><a href="hapusdata.php?id=<?= $mhs['id']; ?>" onclick="return confirm ('Yakinnn?');"><button style="margin-bottom: 12px; background-color :red;" >Hapus</button></a> | 
-            <a href="ubahdata.php/?id=<?= $mhs["id"] ?>"> <button style="margin-bottom: 12px; background-color :blue; color: antiquewhite;"> Edit </button>  </td>
+             <td><img src="images/<?=$mhs['foto'];?> "alt="<?=$mhs['nama'];?>"width="100"></td>
+            <td><?= $mhs["nama"]?></td>
+            <td><?= $mhs["nim"]?></td>
+            <td><?= $mhs["jurusan"]?></td>
+            <td><?= $mhs["nohp"]?></td>
+            <td><a href= "hapusdata.php/?id=<?= $mhs["id"]?> "onclick="return confirm('Yaqueennn?');" ><button style="margin-bottom: 12px; background-color: pink;">Hapus</button></a> | <a href="ubahdata.php/?id=<?= $mhs["id"] ?>"><button style="margin-bottom:12px; background-color: lightblue;" >Edit</button></a></td>
         </tr>
         <?php $i++; } ?>
+       
     </table>
-
 </body>
 </html>
--- function.php
+++ function.php
@@ -25,11 +25,11 @@
     }
 
     return $rows;
-    
+
    }
-   
+
    function tambahmahasiswa($data)
-{
+   {
     global $koneksi;
 
     $nama = $data["nama"];
@@ -46,7 +46,6 @@
 
     // Pindahkan file ke folder images/
     if(move_uploaded_file($temp, $path)) {
-
         // Simpan nama file ke DB
         $query = "INSERT INTO mahasiswa VALUES ('', '$namafile', '$nama', '$nim', '$jurusan', '$nohp')";
         mysqli_query($koneksi, $query);
@@ -54,15 +53,15 @@
     } else {
         return 0; // gagal upload
     }
-}
-
+   }
    
    function hapusdata($id)
    {
     global $koneksi;
-    $query= "DELETE FROM mahasiswa WHERE id = $id";
+
+    $query = "DELETE FROM mahasiswa where id=$id";
     mysqli_query($koneksi, $query);
-    
+
     return mysqli_affected_rows($koneksi);
    }
 
@@ -76,31 +75,68 @@
     $nohp = $data["nohp"];
 
     // Handle upload foto
-    if($_FILES['foto']['error'] === 4) {
-        // Jika tidak ada file yang diupload, tetap gunakan foto lama
-        $namafile = $data["foto_lama"];
+    $file = $_FILES['foto']['name'];
+    $namafile = date ('day_hm'). '_' . $file;
+    $temp = $_FILES['foto']['tmp_name'];
+    $folder = 'images/';
+    $path = $folder . $namafile;
+
+    // Pindahkan file ke folder images/
+    if(move_uploaded_file($temp, $path)) {
+        // Simpan nama file ke DB
+        $query = "UPDATE mahasiswa SET foto = '$namafile', nama='$nama', nim = '$nim', jurusan = '$jurusan', nohp = '$nohp' WHERE id=$id; "; 
+        mysqli_query($koneksi, $query);
+        return mysqli_affected_rows($koneksi);
     } else {
-        // Jika ada file yang diupload, proses upload
-        $file = $_FILES['foto']['name'];
-        $namafile = date ('day_hm'). '_' . $file;
-        $temp = $_FILES['foto']['tmp_name'];
-        $folder = 'images/';
-        $path = $folder . $namafile;
-
-        move_uploaded_file($temp, $path);
+        return 0; // gagal upload
     }
-
-    // Update data ke DB
-    $query = "UPDATE mahasiswa SET 
-    foto='$namafile',
-    nama='$nama', 
-    nim='$nim', 
-    jurusan='$jurusan', 
-    nohp='$nohp' 
-    WHERE id=$id";
-    mysqli_query($koneksi, $query);
-
-    return mysqli_affected_rows($koneksi);
    }
+
+
+   function register($data)
+   {
+        global $koneksi;
+
+        $username = stripslashes($data["username"]);
+        $password1 = $data["password1"];
+        $password2 = $data["password2"];
+
+        $query = "SELECT * FROM user WHERE username='$username'";
+
+        $username_check = mysqli_query($koneksi, $query);
+
+        if(mysqli_num_rows($username_check) > 0 )
+        {
+            return "Username Sudah Terdaftar!";
+        }
+
+        if(!preg_match('/^[a-zA-Z0-9.-_]+$/', $username))
+        {
+            return "Username Tidak Valid";
+        }
+
+        if($password1 !== $password2)
+        {
+            return "Konfirmasi Password Salah!";
+        }
+
+        $encrypt_pass = password_hash($password1, PASSWORD_DEFAULT );
+
+        // return $encrypt_pass;
+
+        $query_insert = "INSERT INTO user (username, password) VALUE ('$username', '$encrypt_pass')";
+
+        if(mysqli_query($koneksi, $query_insert))
+        {
+            return "Register Berhasil";
+        }
+        else
+        {
+            return "Gagal" . mysqli_error($koneksi);
+        }
+
+
+   }
+
 
    ?>
